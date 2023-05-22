22c15d96dddac8fd3a9b1703d283e10a23dce8ba
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-05-22 15:20:17 +0900

    while문 추가

--- 04_Control/src/com/kh/loop/D_While.java
+++ 04_Control/src/com/kh/loop/D_While.java
@@ -121,30 +121,114 @@ public class D_While {
 	 */
 	public void method5() {
 		
-		 int num = 0;
-		 int random = (int)Math.random()*100 + 1;
-		
-		  do  {
-			
-			System.out.print("1과 100사이의 값 입력 > ");
-			 num = sc.nextInt();
-			if(num>random) {
-				System.out.println("더 작은 수를 입력하세요");
-				
-			}else if(num <random){
-				System.out.println("더 큰 수를 입력하세요");
-				
-			}else if(num==random){
-				System.out.println("");
-				
-			}
+		 int input = 0;
+		 int answer = (int)Math.random()*100 + 1;
+		 int count = 0;
+		 
+		 
+		 
+		 while(true) {
+			 count++;
+			 System.out.print("1과 100사이의 값 입력 > ");
+			 input = sc.nextInt();
+			 
+			 if(input>answer) {
+					System.out.println("더 큰 수를 입력하세요");
+					
+				}else if(input <answer){
+					System.out.println("더 작은 수를 입력하세요");
+					
+				}else {
+					System.out.println(count + "번 만에 맞췄습니다.");
+					break;
+		 }
+     }
+}
 	
-		} while(random!=num);
-		
-	}
+		 
 	
+	/*
+	 * 
+	 * 예금액 > 5000
+	 * 
+	 * ----------------------------
+	 * 1. 예금 | 2.출금 | 3.잔고 | 4. 종료
+	 * ----------------------------- 
+	 * 선택 > 1
+	 * 예금액 > 5000
+	 * 
+	 * ----------------------------
+	 * 1. 예금 | 2.출금 | 3.잔고 | 4. 종료
+	 * -----------------------------
+	 * 선택 > 2
+	 * 출금액 > 2000
+	 * 
+	 * ----------------------------
+	 * 1. 예금 | 2.출금 | 3.잔고 | 4. 종료
+	 * -----------------------------
+	 * 선택 > 3
+	 * 잔고 > 3000
+	 * 
+	 * 
+	 * ----------------------------
+	 * 1. 예금 | 2.출금 | 3.잔고 | 4. 종료
+	 * -----------------------------
+	 * 선택 > 4
+	 * 종료 
+	 * 
+	 * 
+	 *
+	 */
+		 
+  public void method6()	{
+	  
+	  
 	
-	
+	  int result = 0; //잔고
+	  int num = 0;
+	  boolean run = true;
+	  
+	  while(run) {
+		 
+		  
+		  System.out.println("---------------------------------");
+		  System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+		  System.out.println("---------------------------------");
+		  System.out.print("선택 > ");
+		  num = Integer.parseInt(sc.nextLine());
+		  // Integer.parseInt :Integer 클래스의 parseInt 메소드 
+		  // String -> int
+		  
+		  switch(num) {
+		  
+		    case 1 : 
+		    	System.out.print("예금액 > ");
+		    	result += Integer.parseInt(sc.nextLine());
+		        break;
+		    
+		    case 2 : 
+				 
+		    	System.out.print("출금액 > " );
+		    	result -= Integer.parseInt(sc.nextLine());
+		        break;
+		    
+		    case 3 :
+		         System.out.println("잔고 > ");
+		         System.out.println(result);
+		         break;
+			  
+		    case 4 : 
+		    	run = false;
+		   
+		  }
+
+	  }
+	  System.out.println("프로그램 종료");
+
+  }
+		 
+		 
+
 	
 	public static void main(String[] args) {
 
@@ -153,7 +237,8 @@ public class D_While {
 //		d.method2();
 //		d.method3();
 //		d.method4();
-		d.method5();
+//		d.method5();
+		d.method6();
 		
 		
 	}
