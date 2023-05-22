561f65a852153fada32bc80aa5f6a76c7e0e582a
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-05-22 09:03:02 +0900

    LoopPractice 추가

--- 04_Control/src/com/kh/loop/LoopPractice.java
+++ 04_Control/src/com/kh/loop/LoopPractice.java
@@ -10,8 +10,11 @@ class LoopPractice {
 	public static void main(String[]args) {
 		LoopPractice c = new LoopPractice();
 //		c.method1();
-		c.method2();
-		
+//		c.method2();
+//		c.method3();
+		c.method4(); 
+//		c.method5();
+//		c.method6();
 	}
 	
 	
@@ -37,6 +40,22 @@ class LoopPractice {
 
     // 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때, 몇까지 더해야 총합이 100 이상 되는지 출력하시오.
     public void method2() {
+    	
+    	int num = 0;
+    	int sum = 0;
+    	int s=1;
+    	int i =0;
+    	while(true) {
+    		s=-s;
+    		num = i*s;
+    		sum+=num;
+    		i++;
+    		if(sum>=100)
+    			break;
+    	}
+    	
+    	System.out.println(num);
+    	System.out.println(sum);
 
     }
 
@@ -49,6 +68,24 @@ class LoopPractice {
 
     */
     public void method3() {
+    	
+    	System.out.print("문자열 : ");
+    	String str = sc.nextLine();
+    	int a = str.length();
+    	
+    	System.out.print("문자 : ");
+    	String str1 = sc.nextLine();
+    	char ch = str1.charAt(0);
+    	int count = 0;
+    	for(int i=0; i<a;i++) {
+    		
+    		if(ch != str.charAt(i)) 
+    			continue;
+    	
+    		   count++;
+    		  
+    	}
+    	 System.out.println(str+ "안에 포함된 "+ ch+ " 개수 : "+ count);
 
     }
 
@@ -63,6 +100,14 @@ class LoopPractice {
         0
      */
     public void method4() {
+    	
+    	int num=0;
+    	
+ 	   do {
+ 		   num = (int)(Math.random()*11) ;
+ 		   System.out.println(num);
+ 	   }while(num!=0);
+ 
 
     }
 
@@ -78,7 +123,35 @@ class LoopPractice {
 
      */
     public void method5() {
+    	
+    	
+    	int a=0;
+    	int b=0;
+    	int c=0;
+    	int d=0;
+    	int e=0;
+    	int f=0;
+    	for(int i=1; i<=10; i++) {
+    		 int num = (int)(Math.random()*6)+1 ;
+    		switch(num) {
+    		case 1: ++a; break;
+    		case 2: ++b; break;
+    		case 3: ++c; break;
+    		case 4: ++d; break;
+    		case 5: ++e; break;
+    		case 6: ++f; break;
+    		
+    		}
+    	}
+    	System.out.println("1:" + a);
+		System.out.println("2:" + b);
+		System.out.println("3:" + c);
+		System.out.println("4:" + d);
+		System.out.println("5:" + e);
+		System.out.println("6:" + f);
+    	
 
+    	
     }
 
     /*
@@ -104,6 +177,87 @@ class LoopPractice {
         이겼습니다 !
     */
     public void method6() {
+    	
+    	System.out.print("당신의 이름을 입력해주세요 : ");
+    	String str = sc.nextLine();
+    	int win = 0;
+    	int lose =0;
+    	int tie =0;
+    	outer:
+    	while(true) {
+    
+    	 int num = (int)(Math.random()*3)+1; 
+    	
+    	
+    	 System.out.print("가위바위보 : ");
+    	 String input = sc.nextLine();
+    	 System.out.print("컴퓨터 : ");
+    	 String com = sc.nextLine();
+    	 System.out.println(str + " : " + input);
+    	
+    	 switch(com) {
+    	   case "바위":
+    		 num =1; break;
+    	   case "가위":
+    		num =2; break;
+    	   case "보":
+    		num =3; break;
+   
+    	    }
+    
+    	 
+    	
+    	  switch(input) {
+    	      case "바위" : 
+    		    if(num == 1) {
+    			 System.out.println("비겼습니다.");
+    			 ++tie;
+    		    }else if(num==2) {
+    			 System.out.println("이겼습니다!");
+    			 ++win;
+    			 break outer;
+    		    }else if(num==3) {
+    			 System.out.println("졌습니다 ㅠㅠ");
+    			 ++lose;
+    		    } 
+    		    break;
+    	
+    	
+    	       case "가위" : 
+    		    if(num == 1) {
+    			 System.out.println("졌습니다 ㅠㅠ");
+    			 ++lose;
+    		    }else if(num==2) {
+    			 System.out.println("비겼습니다.");
+    			 ++tie;
+    		    }else if(num==3) {
+    			 System.out.println("이겼습니다!");
+    			 ++win;
+    			 break outer;
+    		    } 
+    		    break; 
+    	
+    	      case "보" : 
+    		    if(num == 1) {
+    			 System.out.println("이겼습니다!");	
+    			 ++win;
+    			 break outer;
+    		    }else if(num==2) {
+    			 System.out.println("졌습니다 ㅠㅠ");
+    			 ++lose;
+    		    }else if(num==3) {
+    			 System.out.println("비겼습니다.");
+    			 ++tie;
+    		    } 
+    		    break;
+    	
+    	       } 
+    	    System.out.println();
+    
+      } 
+    	System.out.println("이긴 횟수 : "+ win);
+    	System.out.println("진 횟수 : "+ lose);
+    	System.out.println("비긴 횟수 : "+ tie);
 
     }
 
