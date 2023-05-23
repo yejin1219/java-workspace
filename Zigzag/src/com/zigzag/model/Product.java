684eb73da94da9da56faf2c234f32537bfcd2344
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-05-23 23:25:19 +0900

    ZigZag

--- Zigzag/src/com/zigzag/model/Product.java
+++ Zigzag/src/com/zigzag/model/Product.java
@@ -12,7 +12,12 @@ public class Product {
 	public String fileUrl;
 	public Date uploadAt;
 	
-	
+	/*CRUD
+    creat 추가 기능
+    Read 보여주는 기능
+    update 수정 기능
+    delete 계정 삭제 기능
+	 **/
 	
 	 
     // 상품 이미지 업로드
@@ -21,12 +26,24 @@ public class Product {
 	}
 	
     // 상품 목록 
+    public void productList() {
+		
+	}
 	
 	// 상품 1개 보기 
+    public void viewProduct() {
+    	
+    }
 	
 	// 상품 수정
+    public void updateProduct() {
+    	
+    }
 	
 	// 상품 삭제
+    public void deleteProduct() {
+    	
+    }
 	
 	
 	
