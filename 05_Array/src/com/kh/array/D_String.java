50ebedccee3c24a79764a8cac554c8194d298544
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-05-23 15:20:45 +0900

    05_Array 추가

---
+++ 05_Array/src/com/kh/array/D_String.java
@@ -0,0 +1,33 @@
+package com.kh.array;
+
+public class D_String {
+
+	
+	/*
+	 * 문자열 : 문자의 배열, 여러 개의 문자가 배열을 이룬 것이 문자열
+	 * 자바에서는 문자는 작은 따옴표('), 문자열은 큰 따옴표(")를 사용하여 나타낼 수 있음 
+	 * 
+	 * - String,charAt(int index)
+	 *   : 인덱스에 있는 문자를 char 형식으로 반환
+	 *   
+	 * - String.toCharArray(0
+	 *   : 모든 문자가 들어 있는 char[] 형식의 데이터를 반환
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		char[] arr = "Hello,World!".toCharArray();
+		
+		for(char c : arr) {
+			System.out.println(c);
+		}
+
+	}
+	/*
+	 * ArrayIndexOutBoundsException
+	 *  --> 배열의 인덱스 범위를 벗어나서 발생하는 에러!
+	 *  해결책 : 조건식 수정(i < 배열의 길이, i<= 배열의 길이-1)
+	 *
+	 */
+
+}
