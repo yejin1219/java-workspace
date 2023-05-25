cc43575e52fa0fc61a133106234ad6696eeef64a
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-05-26 00:05:28 +0900

    Melon

---
+++ Melon/src/com/melon/controller/AlbumController.java
@@ -0,0 +1,35 @@
+package com.melon.controller;
+
+import com.melon.model.Album;
+
+public class AlbumController {
+
+	
+	
+	// 앨범 업로드
+			public Album upload() {
+				return null;
+			}
+			
+			// 앨범 목록
+			public Album albumList() {
+				return null;
+			}
+			
+			// 앨범 1개 보기 
+			public Album viewMusic() {
+				return null;
+			}
+			
+			
+			// 앨범 수정  
+			public Album  updateMusic() {
+				return null;
+			}
+			
+			
+			// 앨범 삭제 
+			public boolean deleteMusic() {
+				return false;
+			}
+}
--- Melon/src/com/melon/controller/UserController.java
+++ Melon/src/com/melon/controller/UserController.java
@@ -1,35 +1,37 @@
 package com.melon.controller;
 
+import com.melon.model.User;
+
 public class UserController {
 
 	
 	//로그인
-			public void login() {
-				
+			public boolean login() {
+				return false;
 			}
 			
 			//회원가입
-		    public void signUp() {
-				
+		    public boolean signUp() {
+		    	return false;
 			}
 
 			//회원정보 수정
-		    public void updateInformation() {
-				
+		    public User updateInformation() {
+		    	return null;
 			}
 
 			//프로필 설정 
-		    public void settingProfile() {
-				
+		    public User settingProfile() {
+		    	return null;
 			}
 
 			//간편결제 관리
-		    public void settingPay() {
-		    	
+		    public User settingPay() {
+		    	return null;
 		    }
 
 			//계정 삭제 
-		    public void deleteInformation() {
-		    	
+		    public boolean deleteInformation() {
+		    	return false;
 		    }
 }
